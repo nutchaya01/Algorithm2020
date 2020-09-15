4a4c7d90cecd13beca422bdc88e5fecbb164a940
Nutchaya01 <61160140@go.buu.ac.th>
2020-09-15 10:39:23 +0700

    lab 6

--- src/powerMod.java
+++ src/powerMod.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
 
 public class powerMod {
+	public static int mod (int a,int b,int c) {
+		int x = 0;
+		if(b==0) 
+			return 1;
+		
+		if(b%2==0) {
+			x = mod(a,b/2,c);
+			return (x*x)%c;
+		}else {
+			x = mod(a,b/2,c);
+			return (a*(x*x))%c;
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		for (int i=0;i<n;i++) {
+			int a = kb.nextInt();
+			int b = kb.nextInt();
+			int c = kb.nextInt();
+			System.out.println(mod(a,b,c));
+		}
 
 	}
 
